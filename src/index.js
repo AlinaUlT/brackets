bddf9d69c5c2a87af9d8607b30e851138d166ea8
Alina Ulasevich <juliwu2517@gmail.com>
2022-08-12 14:50:03 -0700

    init: brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+	let counter = 0; 
+	let brackets = [];
+
+	for (let i = 0; i < str.length; i++) {
+		for (let [start, end] of bracketsConfig) {
+			if (str[i] === end) {
+				if (brackets[counter - 1] === start) {
+					counter--;
+					brackets.pop();
+				}
+			 else {
+				counter++;
+				brackets.push(str[i]);
+			}
+		} 
+		else {
+			if (str[i] === start) {
+				counter++;
+				brackets.push(str[i]); 
+				}
+			}
+
+		}
+	}
+
+	return brackets.length === 0;
+
+	
 }
+
+//  (((())))()(       , "({[." ,  ")"
